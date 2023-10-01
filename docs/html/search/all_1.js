7fed2f0d27eabe48cad5b52cf3fc7442bc270747
Andrey <agar10@yandex.ru>
2023-10-01 22:04:01 +0300

    Last. +1

---
+++ docs/html/search/all_1.js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['c_20project_20using_20doxygen_20for_20documenting_20code_0',['My first C++ project using Doxygen for documenting code',['../index.html',1,'']]],
+  ['code_1',['My first C++ project using Doxygen for documenting code',['../index.html',1,'']]],
+  ['cpp_20project_20utilizing_20doxygen_2',['my 1st cpp project utilizing Doxygen',['../index.html#intro_sec',1,'']]]
+];
