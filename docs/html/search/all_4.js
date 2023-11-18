aa0a2c01fbf12dbb3c7dcb9ee0fa9b32eb9e64fc
Andrey <agar10@yandex.ru>
2023-11-18 11:32:19 +0300

    Revert "remove all files for review"
    This reverts commit 8b1da3d51de027ed7808177e7f23c630fdcd431d.

---
+++ docs/html/search/all_4.js
@@ -0,0 +1,7 @@
+var searchData=
+[
+  ['main_0',['main',['../main_8cpp.html#ae66f6b31b5ad750f1fe042a706a4e3d4',1,'main.cpp']]],
+  ['main_2ecpp_1',['main.cpp',['../main_8cpp.html',1,'']]],
+  ['my_201st_20cpp_20project_20utilizing_20doxygen_2',['my 1st cpp project utilizing Doxygen',['../index.html#intro_sec',1,'']]],
+  ['my_20first_20c_20project_20using_20doxygen_20for_20documenting_20code_3',['My first C++ project using Doxygen for documenting code',['../index.html',1,'']]]
+];
---
+++ docs/html/search/pages_5.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['using_20doxygen_20for_20documenting_20code_0',['My first C++ project using Doxygen for documenting code',['../index.html',1,'']]]
+];
